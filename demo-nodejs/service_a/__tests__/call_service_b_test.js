a197e1e294699898443263d46f50fe5a38002a53
up1 <somkiat.p@gmail.com>
2020-06-02 23:22:25 +0700

    step 2 :: comsumer testing with pact

--- demo-nodejs/service_a/__tests__/call_service_b_test.js
+++ demo-nodejs/service_a/__tests__/call_service_b_test.js
@@ -1,19 +1,19 @@
 const nock = require('nock')
-const API_PORT = 8081
+const API_PORT = 9002
 const { fetchUser } = require('../service')
 const API_HOST = `http://localhost:${API_PORT}`
 
 describe('Call service B', () => {
   it('Check payload from /user/:name', async () => {
     nock(API_HOST)
-      .get('/user/test user')
+      .get('/user/pact')
       .reply(200, {
         user: {
-          name: 'test user'
+          name: 'pact'
         }
       })
 
-    const response = await fetchUser('test user')
-    expect(response.user.name).toEqual('test user')
+    const response = await fetchUser('pact')
+    expect(response.user.name).toEqual('pact')
   })
 })
---
+++ demo-nodejs/service_a/__tests__/pact_service_b_api.js
@@ -0,0 +1,72 @@
+const {Pact} = require('@pact-foundation/pact')
+const path = require('path')
+const service = require('../service');
+
+
+describe("Service B Test with Pact", () => {
+    let provider
+
+    beforeAll(async () => {
+        provider = new Pact({
+            consumer: 'Service A',
+            provider: 'Service B',
+            port: 9002,
+            log: path.resolve(process.cwd(), 'logs', 'pact.log'),
+            logLevel: "warn",
+            dir: path.resolve(process.cwd(), 'pacts'),
+            spec: 2
+        })
+        await provider.setup()
+    });
+
+    afterEach(async () => {
+        await provider.verify();
+    });
+
+    afterAll(async () => {
+        // Write pact files
+        await provider.finalize()
+    });
+
+    describe("Get data from /user/:name", () => {
+        it('Should be user=pact', async () => {
+            await provider.addInteraction({
+                state: 'User pact existing',
+                uponReceiving: 'a request for user JSON data',
+                withRequest: {
+                    method: 'GET',
+                    path: '/user/pact'
+                },
+                willRespondWith: {
+                    status: 200,
+                    headers: {
+                    'Content-Type': 'application/json; charset=utf-8'
+                    },
+                    body: {
+                        user: {
+                            name: 'pact'
+                        }
+                    }
+                }
+                })
+
+            const result = await service.fetchUser('pact')
+            expect(result.user.name).toEqual('pact')
+        })
+
+        it('Should not found user', async () => {
+            await provider.addInteraction({
+                state: 'User demo not found',
+                uponReceiving: 'a request for user JSON data',
+                withRequest: {
+                    method: 'GET',
+                    path: '/user/demo'
+                },
+                willRespondWith: {
+                    status: 404
+                }
+            })
+            await expect(service.fetchUser('demo')).rejects.toThrow("Not Found");
+        })
+    })
+})
